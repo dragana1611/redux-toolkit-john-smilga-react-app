60eb54c6069bf62f8d8d9b2e4fd04d7d7c7837d5
Dragana Petrovic <dragana.popov-petrovic@badin.rs>
2023-05-03 00:48:46 +0200

    created cartcontainer, cartitem, reducers clearcart

--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -1,16 +1,22 @@
 import { createSlice } from "@reduxjs/toolkit";
+import cartItems from "../../cartItems";
 
 const initialState = {
-  cartItems: [],
-  amount: 0,
+  cartItems: cartItems,
+  amount: 4,
   total: 0,
   isLoading: true,
 };
 
- const cartSlice = createSlice({
+const cartSlice = createSlice({
   name: "cart",
   initialState,
+  reducers: {
+    clearCart: (state) => {
+      state.cartItems = [];
+    },
+  },
 });
 
-
-export default cartSlice.reducer
+export const {clearCart} = cartSlice.actions
+export default cartSlice.reducer;
