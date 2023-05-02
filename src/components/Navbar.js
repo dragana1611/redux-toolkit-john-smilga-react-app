03a107b75c61eb77892632b0305d77c80172c329
Dragana Petrovic <dragana.popov-petrovic@badin.rs>
2023-05-02 22:10:43 +0200

    initial commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { CartIcon } from "../icons";
+import { useSelector } from "react-redux";
+
+const Navbar = () => {
+  return (
+    <nav>
+      <div className="nav-center">
+        <h3>redux toolkit</h3>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
