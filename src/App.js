7ec86792119a448e4b956609825772c2bf1e9b0b
Dragana Petrovic <dragana.popov-petrovic@badin.rs>
2023-05-03 23:13:09 +0200

    added modal, reducers

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 import Navbar from "./components/Navbar";
 import CartContainer from "./components/CartContainer";
+import Modal from "./components/Modal";
+import { useDispatch, useSelector } from "react-redux";
+import { calculateTotals } from "./features/cart/cartSlice";
+import { useEffect } from "react";
 
 function App() {
+  const { cartItems } = useSelector((store) => store.cart);
+  const { isOpen } = useSelector((store) => store.modal);
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(calculateTotals());
+  }, [cartItems]);
+
   return (
     <main>
+      {isOpen && <Modal />}
       <Navbar />
       <CartContainer />
     </main>
