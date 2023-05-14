ce3b8905155a57748313bd2fe01519dc1aaf252a
Dragana Petrovic <dragana.popov-petrovic@badin.rs>
2023-05-14 16:29:21 +0200

    fix bug

--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -47,13 +47,28 @@ const cartSlice = createSlice({
       state.total = total;
     },
   },
-  extraReducers: {
-    [getCartItems.pending]: (state) => (state.isLoading = true),
-    [getCartItems.fulfilled]: (state, action) => {
-      state.isLoading = false;
-      state.cartItems = action.payload;
-    },
-    [getCartItems.rejected]: (state) => (state.isLoading = false),
+  // extraReducers: {
+  //   [getCartItems.pending]: (state) => (state.isLoading = true),
+  //   [getCartItems.fulfilled]: (state, action) => {
+  //     state.isLoading = false;
+  //     state.cartItems = action.payload;
+  //   },
+  //   [getCartItems.rejected]: (state) => (state.isLoading = false),
+  // },
+  extraReducers: (builder) => {
+    builder
+      .addCase(getCartItems.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(getCartItems.fulfilled, (state, action) => {
+        // console.log(action);
+        state.isLoading = false;
+        state.cartItems = action.payload;
+      })
+      .addCase(getCartItems.rejected, (state, action) => {
+        console.log(action);
+        state.isLoading = false;
+      });
   },
 });
 
